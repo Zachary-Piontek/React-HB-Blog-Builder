819a0946d33abc237af3022c47a5ea10dd16680e
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-14 12:18:55 -0700

    state title added and displays

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,17 @@ import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
 
 import './Home.css';
+import { useState } from 'react';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('default title');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title}/>
+      <Editor title={title} setTitle={setTitle}/>
     </main>
   );
 }
