25b9790f502060544252aea858f79b0c94e75ed3
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-14 14:12:37 -0700

    added align state and aligns preview

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,14 +10,15 @@ export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('default title');
   const [subtitle, setSubTitle] = useState('default subtitle');
-  const [font, setFont] = useState('');
+  const [font, setFont] = useState();
+  const [align, setAlign] = useState();
 
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} font={font}/>
-      <Editor setTitle={setTitle} setSubTitle={setSubTitle} setFont={setFont}/>
+      <Preview title={title} subtitle={subtitle} font={font} align={align}/>
+      <Editor setTitle={setTitle} setSubTitle={setSubTitle} setFont={setFont} setAlign={setAlign}/>
     </main>
   );
 }
