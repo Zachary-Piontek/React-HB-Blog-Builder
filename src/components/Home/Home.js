64b420eba4e986dec6562b3c95ee5e020ae949e4
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-14 13:00:03 -0700

    added font state and font styling changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,13 +10,14 @@ export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('default title');
   const [subtitle, setSubTitle] = useState('default subtitle');
+  const [font, setFont] = useState('');
 
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle}/>
-      <Editor setTitle={setTitle} setSubTitle={setSubTitle}/>
+      <Preview title={title} subtitle={subtitle} font={font}/>
+      <Editor setTitle={setTitle} setSubTitle={setSubTitle} setFont={setFont}/>
     </main>
   );
 }
